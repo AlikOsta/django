07eb0edbdc2c60633d11decdb7eefb051051a139
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2024-12-09 20:20:47 +0300

    lesson_54 - создание урлов

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -13,7 +13,16 @@ path - строки, включая слэши '/'
 Пример:
 path('<str:name>/', views.index, name='index')
 '''
+# url posts/..
 
 urlpatterns = [
-    path('<str:name>/', views.index, name='index')
+    #category
+    path('categories/', views.catalog_categories),
+    path('categories/<slug:category_slug>/', views.category_detali, ),
+    # tag
+    path('tags/', views.catalog_tags),
+    path('tags/<slug:tag_slug>/', views.tag_detail),
+    #posts
+    path('', views.catalpg_posts),
+    path('<slug:post_slug>/', views.posts_detail),
 ]
--- python_blog/views.py
+++ python_blog/views.py
@@ -4,6 +4,20 @@ from . import models
 
 
 
-def index(request, name):
-    string = f'Привет {name}'
-    return HttpResponse(string)
+def catalog_categories(request):
+    return HttpResponse('Каталог категорий')
+
+def category_detali(request, category_slug):
+    return HttpResponse(f'Категория {category_slug}')
+
+def catalog_tags(request):
+    return HttpResponse('Каталог тегов')
+
+def tag_detail(request, tag_slug):
+    return HttpResponse(f'Тег {tag_slug}')
+
+def catalpg_posts(request):
+    return HttpResponse('Каталог постов')
+
+def posts_detail(request, post_slug):
+    return HttpResponse(f'Пост {post_slug}')
