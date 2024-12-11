c228d8a88a21d9c17f1735b8ec67a90e4b3e54ce
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2024-12-12 02:05:28 +0300

    lesson_54 - собрали вьюхи на catalog_categories и category_detail

--- python_blog/views.py
+++ python_blog/views.py
@@ -4,15 +4,43 @@ from django.urls import reverse
 
 from . import models
 
-def main(request):
-    return HttpResponse('Главная страница')
+CATEGORIES = [
+    {'slug': 'python', 'name': 'Python'},
+    {'slug': 'django', 'name': 'Django'},
+    {'slug': 'postgresql', 'name': 'PostgreSQL'},
+    {'slug': 'docker', 'name': 'Docker'},
+    {'slug': 'linux', 'name': 'Linux'},
+]
 
+
+def main(request):
+    return HttpResponse("""
+            <h1>Главная страница</h1>
+            <p>Будут все посты</p>
+         """)
 
 def catalog_categories(request):
-    return HttpResponse('Каталог категорий')
+    links = []
+    for category in CATEGORIES:
+        url=reverse('blog:category_detail', args=[category['slug']])
+        links.append(f'<p><a href="{url}">{category["name"]}</a></p>')
+        
+    return HttpResponse(f'''
+        <h1>Каталог категорий<h1>
+        {''.join(links)}
+        <p><a href="{reverse('blog:posts')}">К списку постов</a></p>
+    ''')
 
 def category_detail(request, category_slug):
-    return HttpResponse(f'Категория {category_slug}')
+    category = next((cat for cat in CATEGORIES
+    if cat['slug'] == category_slug), None)
+    if category: name = category['name']
+    else: name = category_slug
+
+    return HttpResponse(f'''
+    <h1>Категория {name}</h1>
+    <p><a href="{reverse('blog:categories')}">К списку категорий</a></p>
+''')
 
 def catalog_tags(request):
     return HttpResponse('Каталог тегов')
