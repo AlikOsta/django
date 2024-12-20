f19756c0bd3c674f529412ad92b600fe39af9f10
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2024-12-20 22:18:24 +0300

    hw_22 - добавил модели

--- python_blog/admin.py
+++ python_blog/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
+from .models import Menu, Categories
 
+admin.site.register(Menu)
+
+admin.site.register(Categories)
--- python_blog/models.py
+++ python_blog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    titel = models.CharField(max_length=100)
+    url_name = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.titel
+    
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    content = models.TextField()
+    hashtags = models.ManyToManyField('Tags')
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='posts/')
+    comments = models.ManyToManyField('Comments')
+    is_published = models.BooleanField(default=False)
+
+class Tags(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Comments(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
