fe9a06c8b5dd104c3c9e721b8771b070d5558b0c
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2024-12-21 02:23:13 +0300

    hw_22 - добавление card include

--- python_blog/views.py
+++ python_blog/views.py
@@ -3,14 +3,6 @@ from django.http import HttpResponse
 from django.urls import reverse
 from . import models
 
-CATEGORIES = [
-    {'slug': 'python', 'name': 'Python'},
-    {'slug': 'django', 'name': 'Django'},
-    {'slug': 'postgresql', 'name': 'PostgreSQL'},
-    {'slug': 'docker', 'name': 'Docker'},
-    {'slug': 'linux', 'name': 'Linux'},
-]
-
 def main(request):
     nemu_items = models.Menu.objects.all()
     posts = models.Post.objects.filter(is_published = True)
@@ -24,27 +16,16 @@ def main(request):
 
 
 def catalog_categories(request):
-    links = []
-    for category in CATEGORIES:
-        url=reverse('blog:category_detail', args=[category['slug']])
-        links.append(f'<p><a href="{url}">{category["name"]}</a></p>')
-        
-    return HttpResponse(f'''
-        <h1>Каталог категорий</h1>
-        {''.join(links)}
-        <p><a href="{reverse('blog:posts')}">К списку постов</a></p>
-    ''')
+    categories = models.Categories.objects.all()
+
+    context= {
+        "categories" : categories
+    }
+
+    return render(request, 'python_blog/categories.html', context)    
 
 def category_detail(request, category_slug):
-    category = next((cat for cat in CATEGORIES
-    if cat['slug'] == category_slug), None)
-    if category: name = category['name']
-    else: name = category_slug
-
-    return HttpResponse(f'''
-    <h1>Категория {name}</h1>
-    <p><a href="{reverse('blog:categories')}">К списку категорий</a></p>
-''')
+    pass
 
 def catalog_tags(request):
     return HttpResponse('Каталог тегов')
