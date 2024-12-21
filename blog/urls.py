3af1ec64c0dde618b7c094c95ea12459e784af55
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2024-12-21 04:52:37 +0300

    hw_22 - подключили bootstrap и настроили стили

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from python_blog import views
@@ -9,4 +10,4 @@ urlpatterns = [
     path('posts/', include('python_blog.urls')),
     path('',  views.main, name="main")
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
